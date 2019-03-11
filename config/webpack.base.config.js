296e5fc1b602207d157ffe3e7ee35682c4ae00c0
Ravinder Mahajan <ravindermahajan890@gmail.com>
2019-03-11 16:47:39 +0530

    Update webpack.base.config.js
    updating js jsx

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -17,7 +17,7 @@ module.exports = env => {
         module: {
           rules: [
             {
-              test: /\.js$/,
+              test: /\.(js|jsx)$/,
               exclude: /node_modules/,
               use: {
                 loader: 'babel-loader'
